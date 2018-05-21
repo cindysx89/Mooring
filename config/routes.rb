18cb8fc26d6f244c2dfc92ae46be7a9c61b2599d
Cindy Xie <cindyxie89@gmail.com>
2018-05-21 16:21:22 +0100

    removed user controller, changed routes, and prepare for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get 'bookings/index'
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'bookings/edit'
-  get 'bookings/destroy'
-  get 'moorings/new'
-  get 'moorings/create'
-  get 'moorings/edit'
-  get 'moorings/destroy'
+
+  resources :bookings
+  resources :moorings, except:[:edit]
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
