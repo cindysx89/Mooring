0de6dc57e64b1abd755c48d93cd5437293305475
Cindy Xie <cindyxie89@gmail.com>
2018-05-21 17:07:51 +0100

    added validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :user_id
+  validates: :start_date, presence: true
+  validates: :end_date, presence: true
 end
--- app/models/mooring.rb
+++ app/models/mooring.rb
@@ -1,3 +1,5 @@
 class Mooring < ApplicationRecord
   belongs_to :user_id
+  validates :location, presence: true
+  validates :price, presence: true
 end
