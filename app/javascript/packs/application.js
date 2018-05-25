802072b0efb7497fcfd25fd1ba943c3e2e77d4f9
Cindy Xie <cindyxie89@gmail.com>
2018-05-25 11:48:05 +0100

    finished boat preloader

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,21 @@ import "bootstrap";
 import flatpickr from "flatpickr";
 import 'flatpickr/dist/flatpickr.min.css';
 flatpickr(".flatpickr", {})
+
+
+
+const loadBoat = (e) => {
+  console.log("you clicked")
+  document.getElementById("ferry").value = "Loading...";
+  const ferryBoat = '<ferry><chimney></chimney><waves></waves></ferry>'
+  document.getElementById("new_booking").insertAdjacentHTML("afterend", ferryBoat);
+    // $(this).unbind('ferry').submit()
+  e.preventDefault();
+  document.getElementById("ferry").removeEventListener('click', loadBoat)
+  setInterval(() => {
+    document.getElementById("new_booking").submit()
+  }, 2000)
+
+}
+
+document.getElementById("ferry").addEventListener("click", loadBoat);
